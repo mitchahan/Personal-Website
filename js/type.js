d2293cb720b5f36861f83d263ca06e8364eb3be5
Mitch Hansen <37028540+mitchahan@users.noreply.github.com>
2018-08-22 22:57:54 -0700

    More Styling and Responsiveness

--- js/type.js
+++ js/type.js
@@ -1,7 +1,7 @@
 var instance = new TypeIt('#intro', {
   speed: 150,
   autoStart: true
-}).pause(1000).type('Mitchell Hansen').pause(1000).delete(10).type(' Hansen').break().pause(1500).type('Computer Science Undergraduate').break().pause(1500).type('Santa Clara University');
+});/*.delete().pause(1000).type('Mitchell Hansen').pause(1000).delete(10).type(' Hansen').break().pause(1500).type('Computer Science Undergraduate').break().pause(1500).type('Santa Clara University');*/
 
 
 var instance = new TypeIt('#intro', options);
