a1dfd40c81269dd863d9094403d30c3ffdb1f0ae
Mitch Hansen <37028540+mitchahan@users.noreply.github.com>
2018-08-23 12:20:54 -0700

    Version 2.0

--- js/type.js
+++ js/type.js
@@ -2,6 +2,3 @@ var instance = new TypeIt('#intro', {
   speed: 150,
   autoStart: true
 });/*.delete().pause(1000).type('Mitchell Hansen').pause(1000).delete(10).type(' Hansen').break().pause(1500).type('Computer Science Undergraduate').break().pause(1500).type('Santa Clara University');*/
-
-
-var instance = new TypeIt('#intro', options);
